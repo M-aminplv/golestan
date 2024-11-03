a40674aaa234d09fceee4fb7349f3f8d0b5f1292
Amin <pahlavanmohammadamin@gmail.com>
2024-11-03 21:17:12 +0330

    little bug fixed

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -26,22 +26,31 @@ const Navbar = () => {
 				className="absolute top-10 left-10  lg:absolute lg:top-14 lg:right-60"
 			/>
 			<div className="bg-black min-h-20">
-				<GiHamburgerMenu size={40} className=' text-white absolute top-2 right-4 lg:hidden  ' onClick={()=>SetNav(!Nav)}/>
-			
-					<ul className={Nav || width>=1024?`flex flex-col lg:flex-row lg: gap-5  p-5 pt-10 justify-center`:`hidden`}>
-						{navBar.map((item) => {
-							
-							return (
-								<li
-									key={item.id}
-									className='lg:border-l-2 border-zinc-600 pr-2 p-3 text-white'>
-									<Link to={`/${item.path}`}>{item.title}</Link>
-								</li>
-							);
-						})}
-						{Nav&& <li className="items-center justify-center p-3 text-white">EN</li>}
-					</ul>
-				
+				<GiHamburgerMenu
+					size={40}
+					className=" text-white absolute top-2 right-4 lg:hidden  "
+					onClick={() => SetNav(!Nav)}
+				/>
+
+				<ul
+					className={
+						Nav || width >= 1024
+							? `flex flex-col lg:flex-row lg: gap-5  p-5 pt-10 justify-center`
+							: `hidden`
+					}>
+					{navBar.map((item) => {
+						return (
+							<li
+								key={item.id}
+								className="lg:border-l-2 border-zinc-600 pr-2 p-3 text-white">
+								<Link to={`/${item.path}`}>{item.title}</Link>
+							</li>
+						);
+					})}
+					{(Nav || width >= 1024) && (
+						<li className="items-center justify-center p-3 text-white">EN</li>
+					)}
+				</ul>
 			</div>
 		</div>
 	);
